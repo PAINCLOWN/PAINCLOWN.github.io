4abf978db233a5b2a62c8b96ac71866aa372abb6
PAINCLOWN <pozz@vip.qq.com>
2018-12-13 19:30:41 +0800

    恢复

---
+++ repo/system/config/autofill.inc.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined("DCRM")) exit();
+define("AUTOFILL_NONAME", "[未命名]");
+define("AUTOFILL_MASTER", "root");
+define("AUTOFILL_EMAIL", "644372842@qq.com");
+define("AUTOFILL_DESCRIPTION", "一个萌萌哒软件包。");
+define("AUTOFILL_SEO", "鐺鐺鐺™(PAINCLOWN)");
+?>
---
+++ repo/system/version.inc.php
@@ -0,0 +1,4 @@
+<?php
+/* Auto-generated version file */
+$_version = '1.7.15.12.30';
+?>
