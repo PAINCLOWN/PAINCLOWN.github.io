bb7a15ce913de2bf43ab6aeb00510ec6c84f9875
PAINCLOWN <pozz@vip.qq.com>
2019-04-29 17:02:28 +0800

    2019年04月29日

---
+++ cj/js/member.js
@@ -0,0 +1,378 @@
+﻿var vip = [
+  '严羽',
+  '周志杰',
+  '余慧阳',
+  '陈娜',
+  '毕德辅',
+]
+var svip = [
+  '董玲',
+  '向攀',
+  '严飞',
+]
+var member = [
+  {
+    "phone": "",
+    "name": "杨文煜"
+  },
+{
+    "phone": "",
+    "name": "朱丹黎"
+  },
+{
+    "phone": "",
+    "name": "陈玉婷"
+  },
+{
+    "phone": "",
+    "name": "金晶"
+  },
+{
+    "phone": "",
+    "name": "熊欣月"
+  },
+{
+    "phone": "",
+    "name": "陈洁"
+  },
+{
+    "phone": "",
+    "name": "王涵"
+  },
+{
+    "phone": "",
+    "name": "段琴琴"
+  },
+{
+    "phone": "",
+    "name": "田丹"
+  },
+{
+    "phone": "",
+    "name": "亢杰"
+  },
+{
+    "phone": "",
+    "name": "周瑜琴"
+  },
+{
+    "phone": "",
+    "name": "向攀"
+  },
+{
+    "phone": "",
+    "name": "严飞"
+  },
+{
+    "phone": "",
+    "name": "郭邦槐"
+  },
+{
+    "phone": "",
+    "name": "胡月"
+  },
+{
+    "phone": "",
+    "name": "毕德辅"
+  },
+{
+    "phone": "",
+    "name": "严羽"
+  },
+{
+    "phone": "",
+    "name": "余慧阳"
+  },
+{
+    "phone": "",
+    "name": "陈娜"
+  },
+{
+    "phone": "",
+    "name": "李陈忱"
+  },
+{
+    "phone": "",
+    "name": "王婷"
+  },
+{
+    "phone": "",
+    "name": "周铭"
+  },
+{
+    "phone": "",
+    "name": "熊文丽"
+  },
+{
+    "phone": "",
+    "name": "董玲"
+  },
+{
+    "phone": "",
+    "name": "余家垚"
+  },
+{
+    "phone": "",
+    "name": "肖敏"
+  },
+{
+    "phone": "",
+    "name": "张雨晴"
+  },
+{
+    "phone": "",
+    "name": "贾倩"
+  },
+{
+    "phone": "",
+    "name": "胡继文"
+  },
+{
+    "phone": "",
+    "name": "常艳红"
+  },
+{
+    "phone": "",
+    "name": "李雨婷"
+  },
+{
+    "phone": "",
+    "name": "李冉"
+  },
+{
+    "phone": "",
+    "name": "史利锐"
+  },
+{
+    "phone": "",
+    "name": "周志杰"
+  },
+{
+    "phone": "",
+    "name": "张甜"
+  },
+{
+    "phone": "",
+    "name": "洪伟"
+  },
+{
+    "phone": "",
+    "name": "林红梅"
+  },
+{
+    "phone": "",
+    "name": "雷星宇"
+  },
+{
+    "phone": "",
+    "name": "武宇清"
+  },
+{
+    "phone": "",
+    "name": "圣亚男"
+  },
+{
+    "phone": "",
+    "name": "范子瑜"
+  },
+{
+    "phone": "",
+    "name": "许文创"
+  },
+{
+    "phone": "",
+    "name": "刘晓慧"
+  },
+{
+    "phone": "",
+    "name": "邹俊奇"
+  },
+{
+    "phone": "",
+    "name": "钟琛"
+  },
+{
+    "phone": "",
+    "name": "寇玲玲"
+  },
+{
+    "phone": "",
+    "name": "潘容"
+  },
+{
+    "phone": "",
+    "name": "度鹃"
+  },
+{
+    "phone": "",
+    "name": "杨若梅"
+  },
+{
+    "phone": "",
+    "name": "杨瑶"
+  },
+{
+    "phone": "",
+    "name": "夏雨霆"
+  },
+{
+    "phone": "",
+    "name": "江天晨"
+  },
+{
+    "phone": "",
+    "name": "袁仕杰"
+  },
+{
+    "phone": "",
+    "name": "江澜"
+  },
+{
+    "phone": "",
+    "name": "韩香"
+  },
+{
+    "phone": "",
+    "name": "马遥"
+  },
+{
+    "phone": "",
+    "name": "张婧"
+  },
+{
+    "phone": "",
+    "name": "苏如意"
+  },
+{
+    "phone": "",
+    "name": "郭锐"
+  },
+{
+    "phone": "",
+    "name": "张习娥"
+  },
+{
+    "phone": "",
+    "name": "骆思敏"
+  },
+{
+    "phone": "",
+    "name": "雷权"
+  },
+{
+    "phone": "",
+    "name": "孙傲"
+  },
+{
+    "phone": "",
+    "name": "韩秀荣"
+  },
+{
+    "phone": "",
+    "name": "周雯"
+  },
+{
+    "phone": "",
+    "name": "张洁"
+  },
+{
+    "phone": "",
+    "name": "童德"
+  },
+{
+    "phone": "",
+    "name": "金洁"
+  },
+{
+    "phone": "",
+    "name": "李泽"
+  },
+{
+    "phone": "",
+    "name": "马骏"
+  },
+{
+    "phone": "",
+    "name": "陈希宁"
+  },
+{
+    "phone": "",
+    "name": "喻诗文"
+  },
+{
+    "phone": "",
+    "name": "谢回春"
+  },
+{
+    "phone": "",
+    "name": "程俐"
+  },
+{
+    "phone": "",
+    "name": "廖雨佳"
+  },
+{
+    "phone": "",
+    "name": "李梦莹"
+  },
+{
+    "phone": "",
+    "name": "胡旻昊"
+  },
+{
+    "phone": "",
+    "name": "马琴"
+  },
+{
+    "phone": "",
+    "name": "赵子君"
+  },
+{
+    "phone": "",
+    "name": "秦康鉴"
+  },
+{
+    "phone": "",
+    "name": "卢潇"
+  },
+{
+    "phone": "",
+    "name": "孙瑛"
+  },
+{
+    "phone": "",
+    "name": "文雯"
+  },
+{
+    "phone": "",
+    "name": "肖宇航"
+  },
+{
+    "phone": "",
+    "name": "魏新杰"
+  },
+{
+    "phone": "",
+    "name": "刘靖雯"
+  },
+{
+    "phone": "",
+    "name": "胡振鄂"
+  },
+{
+    "phone": "",
+    "name": "李娜"
+  },
+{
+    "phone": "",
+    "name": "杨晓红"
+  },
+{
+    "phone": "",
+    "name": "卢轩"
+  },
+{
+    "phone": "",
+    "name": "刘俊"
+  }
+]
