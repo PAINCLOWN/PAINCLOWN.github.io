04041ea112cfbded58793facb886ec3f619f72f7
PAINCLOWN <canyoing@gmail.com>
2019-07-05 13:37:23 +0800

    2019年07月05日

---
+++ index.js
@@ -0,0 +1,8 @@
+
+Spry.Utils.addLoadListener(function() {
+
+			var cnzz_protocol = (("https:" == document.location.protocol) ? " https://": " http://");
+			document.write(unescape("%3Cspan id='cnzz_stat_icon_1275152149'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1275152149%26online%3D2' type='text/javascript'%3E%3C/script%3E"));
+		
+
+});
